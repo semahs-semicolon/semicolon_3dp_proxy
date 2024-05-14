836c3a89aba8c6e7286660db9cf7a2c0eb2d74fd
Ryu Siwoo <thezoot3@gmail.com>
2024-05-14 14:41:34 +0900

    dsf

--- script/setup_nginx.sh
+++ script/setup_nginx.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rm -rf /etc/nginx/nginx.conf
-mv ./../etc/nginx/nginx.conf /etc/nginx/nginx.conf
+mv /home/ubuntu/semicolon_3dp_proxy/etc/nginx/nginx.conf /etc/nginx/nginx.conf
 systemctl restart nginx.service
